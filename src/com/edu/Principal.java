6c7dcd85de7d56e26f3d960505fc47843bb26459
villarrealjavier <villarrealhinojojavier@gmail.com>
2022-02-23 13:34:18 +0100

    Creacion del metodo

--- src/com/edu/Principal.java
+++ src/com/edu/Principal.java
@@ -4,7 +4,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	;
+		
+	}
+	public void metodoSaludar() {
+		System.out.println("Hola papi");
 	}
 
 }
