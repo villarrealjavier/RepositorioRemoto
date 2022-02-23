c2e4686c602754008dadbb0824bc477be5558acc
villarrealjavier <villarrealhinojojavier@gmail.com>
2022-02-23 13:45:44 +0100

    Actualizamos la clase User

--- src/com/edu/User.java
+++ src/com/edu/User.java
@@ -1,5 +1,20 @@
 package com.edu;
 
 public class User {
-
+	private String login;
+	private String password;
+	
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
